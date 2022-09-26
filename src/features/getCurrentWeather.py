dc7fd8f127f70241153414c26f5a403e5a3fb61d
RileyMoore01 <50551379+RileyMoore01@users.noreply.github.com>
2022-09-26 16:36:49 -0500

    Update getCurrentWeather.py

--- src/features/getCurrentWeather.py
+++ src/features/getCurrentWeather.py
@@ -1,2 +1,4 @@
 #API Call Key:     http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}
 #ex.)     api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}
+#    Response: { "cod": 429,"message": "Your account is temporary blocked due to exceeding of requests limitation of your subscription type. 
+#                 Please choose the proper subscription http://openweathermap.org/price" }
