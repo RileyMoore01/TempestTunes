5befe27e3b4da1dd4e1e502043b1c327b5dee4f8
RileyMoore01 <50551379+RileyMoore01@users.noreply.github.com>
2022-09-26 13:01:58 -0500

    Update user.py

--- src/model/user.py
+++ src/model/user.py
@@ -1,11 +1,8 @@
-#Examples
-Id;
-UserName;
-UserPass;
-EmailAddress;
-TelephoneNo;
-Address1;
-Address2;
-City;
-ZipCode;
-
+#Examples in c# bc idk how to declare classes in python lol
+public class Record() {
+  public int Id {get; set;}
+  public string UserName {get; set;}
+  public string UserPass {get; set;}
+  public string EmailAddress {get; set;}
+  public DateTime LastUpdatedTimestamp {get; set;}
+}
