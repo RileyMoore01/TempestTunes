b8b78e33fe83016283f284738824c7b2dd7c6da1
RileyMoore01 <50551379+RileyMoore01@users.noreply.github.com>
2022-10-03 16:40:04 -0500

    Update getCurrentWeather.py

--- src/features/getCurrentWeather.py
+++ src/features/getCurrentWeather.py
@@ -1,15 +1,13 @@
-#API Call Key:     http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}
-#ex.)     api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}
-#    Response: { "cod": 429,"message": "Your account is temporary blocked due to exceeding of requests limitation of your subscription type. 
-#                 Please choose the proper subscription http://openweathermap.org/price" }
-
 # import required modules
 import requests, json
 
-OPEN_WEATHER_MAP_APIKEY = 'fed200574f31448d3c4ef74409fc60bf'
-zip = 76210
+def get_zipcode():
+    #connect html page for the user to enter their current zip code
 
 def get_weather_data_by_location():
+    #####################################
+    #   Get lat, lon, and city name     #
+    #####################################
     url = f'http://api.openweathermap.org/geo/1.0/zip?zip={zip}&appid={OPEN_WEATHER_MAP_APIKEY}'
     response = requests.get(url)
     data = json.loads(response.text)
@@ -18,13 +16,16 @@ def get_weather_data_by_location():
     lon = data["lon"]
     city = data["name"]
 
+    #####################################
+    #        Get current weather       #
+    #####################################
     url2 = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={OPEN_WEATHER_MAP_APIKEY}'
     cityResponse = requests.get(url2)
     weatherData = json.loads(cityResponse.text)
 
     return weatherData
     if response.status_code == 200:
-        return response.json()
+        return response.json() 
     else:
         return None
 
