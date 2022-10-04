e4bb5fbac13baaf7e496f0981f0add7502811651
zebu-lord <50551379+zebu-lord@users.noreply.github.com>
2022-10-03 23:46:35 -0500

    Db call
    Created database
    Added User table
    Set new password for database

--- src/controller/controllers/controller.py
+++ src/controller/controllers/controller.py
@@ -1,3 +1,23 @@
-#Example
-#Can use a browseDataModel | mappingProfile | etc. (extensions of the model for logic or featurability)
-#Will be used to call all the feautres only, no declarations
+#pip install mysql-connector
+import mysql.connector as sql
+
+db = sql.connect(host='localhost',
+                user='root',
+                password='password.',
+                database='testDb')
+
+myCursor = db.cursor()
+##########################
+#    Table creation      #
+##########################
+# myCursor.execute('CREATE DATABASE testDb')
+# myCursor.execute("CREATE TABLE User (PID int PRIMARY KEY AUTO_INCREMENT, user_name varchar(50), password varchar(50))")
+
+##########################
+#    Logic Handeling     #
+##########################
+myCursor.execute("set password for 'testDb'@'localhost' = PASSWORD('pass')")
+
+myCursor.execute('DESCRIBE User')
+# mycursor.execute("insert into User (PID, user_name, password) values (1, 'riley', 'pass')")
+
