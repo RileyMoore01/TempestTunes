64617a408cb5f4808c80d002e223fd7c5a328a64
zebu-lord <50551379+zebu-lord@users.noreply.github.com>
2022-10-03 23:50:36 -0500

    Added comments

--- src/controller/controllers/controller.py
+++ src/controller/controllers/controller.py
@@ -1,4 +1,8 @@
 #pip install mysql-connector
+
+#Still needed: 
+#   Connection to html to add the user input for user and password
+
 import mysql.connector as sql
 
 db = sql.connect(host='localhost',
@@ -12,6 +16,13 @@ myCursor = db.cursor()
 ##########################
 # myCursor.execute('CREATE DATABASE testDb')
 # myCursor.execute("CREATE TABLE User (PID int PRIMARY KEY AUTO_INCREMENT, user_name varchar(50), password varchar(50))")
+
+############################        
+# PID |  USER  |  PASS     #        PID must be a unqiue value in the table
+# -------------------------#        
+# 0  |  test1  |  pass     #        All attributes within the table wil are non-nullable
+# 1  |  test2  |  pass     #
+#############################
 
 ##########################
 #    Logic Handeling     #
