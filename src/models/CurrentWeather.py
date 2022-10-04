6708bccccb22dfb7740ea05b9d56c604a8f3d9b3
zebu-lord <50551379+zebu-lord@users.noreply.github.com>
2022-10-03 23:45:56 -0500

    Cleaned up code

--- src/models/CurrentWeather.py
+++ src/models/CurrentWeather.py
@@ -1,11 +1,11 @@
-class CurrentWeather:
-    Lat = 0.0;
-    Lon = 0.0;
-    City = "";
-    Temp = 0.0;
-    TempMin = 0.0;
-    TempMax = 0.0;
-    AtmoPressure = 0;
-    Humidity = 0;
-    Visibility = 0;
-    Description = "";
+class CurrentWeather():
+    Lat = 0.0
+    Lon = 0.0
+    City = ""
+    Temp = 0.0
+    TempMin = 0.0
+    TempMax = 0.0
+    AtmoPressure = 0
+    Humidity = 0
+    Visibility = 0
+    Description = ""
