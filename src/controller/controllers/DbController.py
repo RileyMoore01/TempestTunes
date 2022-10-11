e9cdef3b537070197c39eae2dcfa1f8339e14c3e
RileyMoore01 <xrileyxmoorex@outlook.com>
2022-10-10 20:33:45 -0500

    Flask

--- src/controller/controllers/DbController.py
+++ src/controller/controllers/DbController.py
@@ -1,50 +1,59 @@
 #pip install mysql-connector
 import mysql.connector as sql
 
-##########################
-#       Db Info          #
-##########################
-#Host: 127.0.0.1
-#Port: 3306 
-#User: root
-#Pass: pass
-db = sql.connect(host='127.0.0.1',
-                user='root',
-                password='pass',
-                database='testDb')
+def testConnection():
+    try:
+        ##########################
+        #       Db Info          #
+        ##########################
+        #Host: 127.0.0.1
+        #Port: 3306 
+        #User: root
+        #Pass: pass
+        db = sql.connect(host='127.0.0.1',
+                        user='root',
+                        password='pass',
+                        database='testDb')
 
-myCursor = db.cursor()
-
-
-
-##########################
-#    Table creation      #
-##########################
-
-#Only needs to be ran once for creation of the DB
-# myCursor.execute('CREATE DATABASE testDb')
-# myCursor.execute("CREATE TABLE User (PID int PRIMARY KEY AUTO_INCREMENT, user_name varchar(50), password varchar(50))")
-# myCursor.execute("SET PASSWORD FOR 'root'@'localhost' = 'pass'")
-
-############################        Example model
-# PID |  USER  |  PASS     #        PID must be a unqiue value in the table
-# -------------------------#        
-# 0  |  test1  |  pass     #        All attributes within the table are non-nullable
-# 1  |  test2  |  pass     #
-############################
+        myCursor = db.cursor()
+        print("*****MySql database connection is open*****")
 
 
+        ##########################
+        #    Table creation      #
+        ##########################
 
-##########################
-#    Logic Handeling     #
-##########################
+        #Only needs to be ran once for creation of the DB
+        # myCursor.execute('CREATE DATABASE testDb')
+        # myCursor.execute("CREATE TABLE User (PID int PRIMARY KEY AUTO_INCREMENT, user_name varchar(50), password varchar(50))")
+        # myCursor.execute("SET PASSWORD FOR 'root'@'localhost' = 'pass'")
 
-myCursor.execute("show databases")
-for x in myCursor:
-    print(x)
+        ############################        Example model
+        # PID |  USER  |  PASS     #        PID must be a unqiue value in the table
+        # -------------------------#        
+        # 0  |  test1  |  pass     #        All attributes within the table are non-nullable
+        # 1  |  test2  |  pass     #
+        ############################
 
-myCursor.execute("Use testDb;")
-# myCursor.execute("""insert into user (PID, user_name, password) values ('2', 'riley', 'pass')""")
 
-print(myCursor.execute("""select * from user where PID = '1'"""))
+        ##########################
+        #    Logic Handeling     #
+        ##########################
 
+        print("--------- Current Databases ---------")
+        myCursor.execute("show databases")
+        for x in myCursor:
+            print(x)
+        print("--------- End of Databases ---------")
+
+        print(myCursor.execute("DESCRIBE user"))
+        print(myCursor.execute("""SELECT * FROM user"""))
+
+    except sql.Error as e:
+        print("*****Error reading data from MySql database*****", e)
+
+    finally:
+        if db.is_connected():
+            db.close()
+            myCursor.close()
+            print("*****MySql database connection closed*****")
---
+++ src/controller/controllers/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+be user"))
+    print(myCursor.execute("""select * from user where PID = '1'"""))
