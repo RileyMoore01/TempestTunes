c340c8a20052ef7e2392f151c316f309565d2c58
RileyMoore01 <xrileyxmoorex@outlook.com>
2022-10-27 21:52:23 -0500

    working db

--- src/controller/controllers/DbController.py
+++ src/controller/controllers/DbController.py
@@ -26,11 +26,22 @@ def testConnection():
         ##########################
         #    Table creation      #
         ##########################
+        # Only needs to be ran once for creation of the DB
 
-        #Only needs to be ran once for creation of the DB
+
         # myCursor.execute('CREATE DATABASE testDb')
         # myCursor.execute("CREATE TABLE User (PID int PRIMARY KEY AUTO_INCREMENT, user_name varchar(50), password varchar(50))")
         # myCursor.execute("SET PASSWORD FOR 'root'@'localhost' = 'pass'")
+
+        # CREATE TABLE `testdb`.`user` (
+            # `PID` INT NOT NULL AUTO_INCREMENT,
+            # `user_name` VARCHAR(45) NOT NULL,
+            # `password` VARCHAR(45) NULL,
+            # `mSunny` INT NOT NULL,
+            # `mSnowy` INT NOT NULL,
+            # `mRainy` INT NOT NULL,
+            # `mWindy` INT NOT NULL,
+            # PRIMARY KEY (`PID`));
 
         print("--------- Current Databases ---------")
         cursor.execute("show databases")
@@ -59,11 +70,11 @@ def AddNewUser():
         cursor = db.cursor(buffered=True)
 
         table = (
-            "SELECT * from spUsername"
+            "SELECT * from user"
         )
 
         insertStatement = (
-            "INSERT INTO spUsername (user_name) "
+            "INSERT INTO user (user_name) "
             "VALUES('first')"
         )
         data = ["..."]      # INSERT USER INPUT HERE ---------------------------
