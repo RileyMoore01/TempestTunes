3c230fac323faf8a9a69f000dc39125c49f149c4
RileyMoore01 <xrileyxmoorex@outlook.com>
2022-11-07 21:40:28 -0600

    Final db controller

--- src/controller/controllers/DbController.py
+++ src/controller/controllers/DbController.py
@@ -1,14 +1,20 @@
-#pip install mysql-connector
-
+# pip install mysql-connector
 import mysql.connector as sql
 
+# Global Properties
+MAX_ROW_ID = 0
+
+
+#
+#       USER INPUT ON LINES 61 - 69 NEEDED
+#
+
+
+
 def testConnection():
-    
     try:
 
-        ##########################
-        #       Db Info          #
-        ##########################
+        # --- DATABASE INFORMATION ---
         #Host: 127.0.0.1
         #Port: 3306 
         #User: root
@@ -20,28 +26,12 @@ def testConnection():
 
         cursor = db.cursor(buffered=True)
         print("***** MySql database connection is open *****")
+        
 
-
-
-        ##########################
-        #    Table creation      #
-        ##########################
         # Only needs to be ran once for creation of the DB
-
-
         # myCursor.execute('CREATE DATABASE testDb')
-        # myCursor.execute("CREATE TABLE User (PID int PRIMARY KEY AUTO_INCREMENT, user_name varchar(50), password varchar(50))")
         # myCursor.execute("SET PASSWORD FOR 'root'@'localhost' = 'pass'")
 
-        # CREATE TABLE `testdb`.`user` (
-            # `PID` INT NOT NULL AUTO_INCREMENT,
-            # `user_name` VARCHAR(45) NOT NULL,
-            # `password` VARCHAR(45) NULL,
-            # `mSunny` INT NOT NULL,
-            # `mSnowy` INT NOT NULL,
-            # `mRainy` INT NOT NULL,
-            # `mWindy` INT NOT NULL,
-            # PRIMARY KEY (`PID`));
 
         print("--------- Current Databases ---------")
         cursor.execute("show databases")
@@ -61,38 +51,53 @@ def testConnection():
             print("***** MySql database connection closed *****")
 
 
+
+
+
 def AddNewUser():
         db = sql.connect(host='127.0.0.1',
                         user='root',
                         password='pass',
                         database='testDb')
 
+        if db.is_connected():
+            cursor = db.cursor(buffered=True)
+            
+            userName = 'user_name'
+            password = 'password'
+            mSunny = 1
+            mRainy = 2
+            mSnowy = 3
+            mWindy = 4
+            Location = '79401'
+
+            insertStatement = "insert into user (user_name, password, mSunny, mWindy, mSnowy, mRainy, Location, row_id) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)"
+            data = (userName, password, mSunny, mWindy, mSnowy, mRainy, Location, MAX_ROW_ID)
+
+            cursor.execute(insertStatement, data)
+            db.commit()
+            cursor.close()
+            db.close()
+
+
+
+
+
+
+def showAllRecords():
+    db = sql.connect(host='127.0.0.1',
+                        user='root',
+                        password='pass',
+                        database='testDb')
+
+    if db.is_connected():
         cursor = db.cursor(buffered=True)
+        statement = "select * from user"
+        cursor.execute(statement)
+        result = cursor.fetchall()
+        print(result)
 
-        table = (
-            "SELECT * from user"
-        )
-        
-        # PID = cursor.execute("Select MAX PID from user")
-        userName = "testMySqlConnection"
-        password = "pass"
-        mSunny = 1
-        mRainy = 2
-        mSnowy = 3
-        mWindy = 4
-        Location = "79401"
-
-        insertStatement = (
-            "insert into user (user_name, password, mSunny, mWindy, mSnowy, mRainy, Location)  "
-            f"VALUES({userName}, {password}, {mSunny},{mRainy}, {mSnowy}, {mWindy}, {Location})"
-        )
-        
-        
-        data = ["testMySqlConnection", "pass", mSunny, mSnowy, mRainy, mRainy, Location]      # INSERT USER INPUT HERE ---------------------------
-
-        # Execute, Commit, and Close database
-        cursor.execute(insertStatement, data)
-        db.commit()
+        cursor.close()
         db.close()
 
 
@@ -100,4 +105,127 @@ def AddNewUser():
 
 
 
-AddNewUser()
+def getMaxPid():
+    db = sql.connect(host='127.0.0.1',
+                        user='root',
+                        password='pass',
+                        database='testDb')
+
+    if db.is_connected():
+        cursor = db.cursor(buffered=True)
+        cursor.execute("select MAX(row_id) as RowId from user;")
+
+        arr  = cursor.fetchone()
+        for i in arr:
+            result = i
+
+        cursor.close()
+        db.close()
+
+        MAX_ROW_ID = result
+
+
+
+
+def updateWeatherStatus():
+        db = sql.connect(host='127.0.0.1',
+                    user='root',
+                    password='pass',
+                    database='testDb')
+
+        if db.is_connected():
+            cursor = db.cursor(buffered=True)
+            
+            rowId = 5
+            mSunny = 4
+            mRainy = 4
+            mSnowy = 4
+            mWindy = 4
+            Location = '76210'
+
+            updateStatement = "update user set mSunny = %s, mWindy = %s, mSnowy=%s, mRainy=%s where row_id=%s"
+            data = (mSunny, mWindy, mSnowy, mRainy, rowId)
+
+            cursor.execute(updateStatement, data)
+            db.commit()
+            cursor.close()
+            db.close()
+
+
+def updateLocation():
+        db = sql.connect(host='127.0.0.1',
+                user='root',
+                password='pass',
+                database='testDb')
+
+        if db.is_connected():
+            cursor = db.cursor(buffered=True)
+            
+            rowId = 5
+            Location = '76219'
+
+            updateStatement = "update user set Location=%s where row_id=%s"
+            data = (Location, rowId)
+
+            cursor.execute(updateStatement, data)
+            db.commit()
+            cursor.close()
+            db.close()
+
+def updateUsername():
+        db = sql.connect(host='127.0.0.1',
+                user='root',
+                password='pass',
+                database='testDb')
+
+        if db.is_connected():
+            cursor = db.cursor(buffered=True)
+            
+            rowId = 4
+            userName = 'testUpdate'
+
+            updateStatement = "update user set user_name=%s where row_id=%s"
+            data = (userName, rowId)
+
+            cursor.execute(updateStatement, data)
+            db.commit()
+            cursor.close()
+            db.close()
+
+def updatePassword():
+        db = sql.connect(host='127.0.0.1',
+                user='root',
+                password='pass',
+                database='testDb')
+
+        if db.is_connected():
+            cursor = db.cursor(buffered=True)
+            
+            rowId = 4
+            password = 'newPasswordUpdate'
+
+            updateStatement = "update user set password=%s where row_id=%s"
+            data = (password, rowId)
+
+            cursor.execute(updateStatement, data)
+            db.commit()
+            cursor.close()
+            db.close()
+
+
+
+# -------------------------------------
+#          Function Calls            --
+# -------------------------------------
+
+# testConnection()
+
+# AddNewUser()
+# getMaxPid()
+
+# updateWeatherStatus()
+# updateLocation()
+# updateUsername()
+# updatePassword()
+
+# showAllRecords()
