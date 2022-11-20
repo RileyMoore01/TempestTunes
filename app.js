0acf414b0bea4949d5bed8079553e1880f20f9b1
RileyMoore01 <xrileyxmoorex@outlook.com>
2022-11-20 00:10:39 -0600

    addedd new api calls

--- app.js
+++ app.js
@@ -398,3 +398,35 @@ function getCurrentWeather(form){
     var sunny = form.sunny.value;
     alert("You chose: " + sunny);
 }
+
+
+function getWeather( cityID ) {
+    var key = 'fed200574f31448d3c4ef74409fc60bf';
+    fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID+ '&appid=' + key)  
+    .then(function(resp) { return resp.json() }) // Convert data to json
+    .then(function(data) {
+        mapWeather(data); // Call drawWeather
+    })
+    .catch(function() {
+
+    });
+}
+
+
+function mapWeather( d ) {
+    var celcius = Math.round(parseFloat(d.main.temp)-273.15);
+    var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
+    var description = d.weather[0].description;
+    
+    document.getElementById('description').innerHTML = description;
+    document.getElementById('temp').innerHTML = fahrenheit + '&deg;';
+    document.getElementById('location').innerHTML = d.name;
+    
+    if( description.indexOf('rain') > 0 ) {
+    document.body.className = 'rainy';
+    } else if( description.indexOf('cloud') > 0 ) {
+        document.body.className = 'cloudy';
+    } else if( description.indexOf('sunny') > 0 ) {
+        document.body.className = 'sunny';
+    }
+}
